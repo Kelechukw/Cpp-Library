7f89475e756fd6e555d830390123ab72f29227d5
Kelechukw <73535689+Kelechukw@users.noreply.github.com>
2020-12-21 06:02:11 +0000

    Update CalcServProvider.php

--- src/CalcServProvider.php
+++ src/CalcServProvider.php
@@ -9,14 +9,14 @@ class CalculatorServiceProvider extends ServiceProvider
     
     public function boot()
     {
-        //
+        //Directory route
         include __DIR__.'/routes.php';
     }
 
     
     public function register()
     {
-        //
+        //register services
         $this->app->make('Kelechukw\Calculator\CC');
          $this->loadViewsFrom(__DIR__.'/views', 'calculator');
     }
